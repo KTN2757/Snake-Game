13f83c285e443a85e692aa12f9fe3e07ba542544
KTN__ <kritagyadahal588@gmail.com>
2024-03-24 15:54:47 +0545

    Added Proper Movement and Animation

--- Snake/snake_head.gd
+++ Snake/snake_head.gd
@@ -1,11 +1,22 @@
 extends CharacterBody2D
+@onready var animation_tree = $AnimationTree
 
 const SPEED = 4800
-
 var input_vec = Vector2.ZERO
+
 func _physics_process(delta):
 	input_vec = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity.x = 1
-	velocity = input_vec * delta * SPEED
-	print(velocity)
+	if input_vec.x == 1:
+		velocity = (input_vec * delta * SPEED) / 32
+		velocity += (input_vec * delta * SPEED)
+	if input_vec.x == - 1:
+		velocity = (input_vec * delta * SPEED) / 32
+		velocity += (input_vec * delta * SPEED)
+	if input_vec.y == 1:
+		velocity = (input_vec * delta * SPEED) / 32
+		velocity += (input_vec * delta * SPEED)
+	if input_vec.y == - 1:
+		velocity = (input_vec * delta * SPEED) / 32
+		velocity += (input_vec * delta * SPEED)
+	animation_tree["parameters/Run/blend_position"] = velocity
 	move_and_slide()
